a4afbd316e56957b468633eec605d9c7dd5c1972
Anish shetty k <anishshettyk@gmail.com>
2021-03-31 16:33:28 +0530

    user stats added

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -65,11 +65,56 @@ const UserMoreInfo = styled.section`
   }
 `;
 
+const UserNumbers = styled.section`
+  ${mixins.flexCenter};
+  margin-top: 30px;
+  div {
+    margin: 0px 20px;
+    padding: 10px 20px;
+    border: 3px dashed ${colors.orange};
+    min-width: 300px;
+    p {
+      line-height: 0;
+      color: ${colors.lightBlue};
+      font-weight: bold;
+      position: relative;
+      text-align: center;
+      &:before,
+      &:after {
+        position: absolute;
+        content: '';
+        width: 30%;
+        height: 2px;
+        background-color: ${colors.orange};
+        opacity: 0.4;
+        top: 50%;
+      }
+      &:before {
+        left: 10%;
+      }
+      &:after {
+        right: 10%;
+      }
+    }
+    h2 {
+      text-align: center;
+      text-transform: uppercase;
+    }
+    h1 {
+      color: ${colors.orange};
+      text-align: center;
+      font-size: 40px;
+      font-weight: bolder;
+    }
+  }
+`;
+
 const UserInfo = ({ userData }) => {
   const getFormattedDate = (date) => {
     const dateFormatted = new Date(date).toLocaleDateString('en-US', { month: 'long', date: 'numeric', year: 'numeric' });
     return dateFormatted;
   };
+  console.log(userData);
   return (
     <StyledUserContainer>
       <UserContainer>
@@ -110,6 +155,23 @@ const UserInfo = ({ userData }) => {
           </div>
         )}
       </UserMoreInfo>
+      <UserNumbers>
+        <div>
+          <p>Total</p>
+          <h2>REPOSITORIES.</h2>
+          <h1>{userData?.public_repos || 0}</h1>
+        </div>
+        <div>
+          <p>Total</p>
+          <h2>Followers.</h2>
+          <h1>{userData?.followers || 0}</h1>
+        </div>
+        <div>
+          <p>Total</p>
+          <h2>Following.</h2>
+          <h1>{userData?.following || 0}</h1>
+        </div>
+      </UserNumbers>
     </StyledUserContainer>
   );
 };
